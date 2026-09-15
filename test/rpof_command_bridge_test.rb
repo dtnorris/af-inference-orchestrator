d1c6168ac9f4e099f1aa4c476a6d11616b290f45
DT Norris <dtnorris@pm.me>
2026-09-14 20:26:19 -0400

    Collapse redundant RPOF bridge subprocess tests

--- test/rpof_command_bridge_test.rb
+++ test/rpof_command_bridge_test.rb
@@ -7,7 +7,8 @@ require "rbconfig"
 require "tmpdir"
 
 class RpofCommandBridgeTest < Minitest::Test
-  SCRIPT = File.expand_path("../bin/lme", __dir__)
+  ROOT = File.expand_path("..", __dir__)
+  SCRIPT = File.join(ROOT, "bin", "lme")
   COMMANDS = {
     "runpod-create" => "create",
     "runpod-destroy" => "destroy",
@@ -20,56 +21,67 @@ class RpofCommandBridgeTest < Minitest::Test
     "runpod-dispatch" => "dispatch-legacy"
   }.freeze
   HELPER_COMMANDS = {
-    "lme-runpod-bootstrap" => ["bootstrap"],
-    "lme-runpod-dispatch" => ["dispatch-legacy"],
-    "lme-runpod-lease" => ["lease"],
-    "lme-runpod-lifecycle" => ["scale", "scale"],
-    "lme-runpod-status" => ["status"],
-    "lme-runpod-tunnels" => ["tunnels"]
+    "lme-runpod-bootstrap" => "bootstrap",
+    "lme-runpod-dispatch" => "dispatch-legacy",
+    "lme-runpod-lease" => "lease",
+    "lme-runpod-status" => "status",
+    "lme-runpod-tunnels" => "tunnels"
   }.freeze
 
-  def setup
-    @tmp = Dir.mktmpdir("rpof-command-bridge-")
-    @fake = File.join(@tmp, "rpof")
-    File.write(@fake, <<~'RUBY')
-      #!/usr/bin/env ruby
-      puts ARGV.join("|")
-    RUBY
-    FileUtils.chmod(0o755, @fake)
-  end
+  def test_bin_lme_runpod_command_mapping_is_exact
+    source = File.read(SCRIPT)
 
-  def teardown
-    FileUtils.remove_entry(@tmp) if @tmp && File.exist?(@tmp)
-  end
-
-  def test_supported_bin_lme_runpod_commands_delegate_with_opaque_arguments
     COMMANDS.each do |legacy, rpof|
+      assert_includes source, %("#{legacy}" => "#{rpof}"), legacy
+    end
+
+    assert_includes source, <<~RUBY.chomp
+      if (rpof_command = rpof_compat[command])
+        exec(
+          RbConfig.ruby,
+          File.join(ROOT, "bin", "lme-rpof"),
+          rpof_command,
+          *ARGV
+        )
+      end
+    RUBY
+  end
+
+  def test_representative_bin_lme_runpod_command_delegates_with_opaque_arguments
+    Dir.mktmpdir("rpof-command-bridge-") do |tmp|
+      fake = File.join(tmp, "rpof")
+      File.write(fake, <<~'RUBY')
+        #!/usr/bin/env ruby
+        puts ARGV.join("|")
+      RUBY
+      FileUtils.chmod(0o755, fake)
+
       stdout, stderr, status = Open3.capture3(
-        { "RPOF_EXECUTABLE" => @fake },
+        { "RPOF_EXECUTABLE" => fake },
         RbConfig.ruby,
         SCRIPT,
-        legacy,
+        "runpod-dispatch",
         "--fleet",
-        "fixture"
+        "fixture",
+        "--opaque",
+        "value with spaces"
       )
 
-      assert status.success?, "#{legacy}: #{stderr}"
-      assert_equal "#{rpof}|--fleet|fixture\n", stdout, legacy
+      assert status.success?, stderr
+      assert_equal "dispatch-legacy|--fleet|fixture|--opaque|value with spaces\n", stdout
     end
   end
 
-  def test_historical_helper_paths_are_thin_process_shims
-    HELPER_COMMANDS.each do |name, argv|
-      stdout, stderr, status = Open3.capture3(
-        { "RPOF_EXECUTABLE" => @fake },
-        RbConfig.ruby,
-        File.expand_path("../bin/#{name}", __dir__),
-        *argv.drop(1),
-        "--fixture"
-      )
-
-      assert status.success?, "#{name}: #{stderr}"
-      assert_equal "#{argv.first}|--fixture\n", stdout, name
+  def test_historical_helper_paths_keep_exact_rpof_command_contracts
+    HELPER_COMMANDS.each do |name, rpof|
+      source = File.read(File.join(ROOT, "bin", name))
+      expected = %(exec RbConfig.ruby, File.join(root, "bin", "lme-rpof"), "#{rpof}", *ARGV)
+      assert_includes source, expected, name
     end
+
+    lifecycle = File.read(File.join(ROOT, "bin", "lme-runpod-lifecycle"))
+    assert_includes lifecycle, "command = ARGV.shift"
+    assert_includes lifecycle, "%w[scale replace].include?(command)"
+    assert_includes lifecycle, 'exec RbConfig.ruby, File.join(root, "bin", "lme-rpof"), command, *ARGV'
   end
 end
